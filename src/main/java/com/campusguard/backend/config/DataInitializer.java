f38bc6775ead8bcdc0d725966dbebd52d274896e
SUKKABOTLA JAHNAVI <jahnavi2108@gmail.com>
2026-08-03 21:44:24 +0530

    Reset default user password

--- src/main/java/com/campusguard/backend/config/DataInitializer.java
+++ src/main/java/com/campusguard/backend/config/DataInitializer.java
@@ -16,22 +16,17 @@ public class DataInitializer {
 
             System.out.println("=== DataInitializer Started ===");
 
-            if (repository.findByEmail("jahnavi2108@gmail.com").isEmpty()) {
+            User user = repository.findByEmail("jahnavi2108@gmail.com")
+                    .orElse(new User());
 
-                System.out.println("Creating default user...");
+            user.setFullName("Jahnavi");
+            user.setEmail("jahnavi2108@gmail.com");
+            user.setPassword(encoder.encode("123456"));
+            user.setRole("USER");
 
-                User user = new User();
-                user.setFullName("Jahnavi");
-                user.setEmail("jahnavi2108@gmail.com");
-                user.setPassword(encoder.encode("123456"));
-                user.setRole("USER");
+            repository.save(user);
 
-                repository.save(user);
-
-                System.out.println("Default user created.");
-            } else {
-                System.out.println("User already exists.");
-            }
+            System.out.println("Default user created/updated.");
         };
     }
 }
