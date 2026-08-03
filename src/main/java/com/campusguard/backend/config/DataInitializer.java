262af7daa8457648de71ec2c56a7e8890507f10e
SUKKABOTLA JAHNAVI <jahnavi2108@gmail.com>
2026-08-03 19:27:14 +0530

    Add default user initializer

---
+++ src/main/java/com/campusguard/backend/config/DataInitializer.java
@@ -0,0 +1,30 @@
+package com.campusguard.backend.config;
+
+import com.campusguard.backend.entity.User;
+import com.campusguard.backend.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class DataInitializer {
+
+    @Bean
+    CommandLineRunner init(UserRepository repository, PasswordEncoder encoder) {
+        return args -> {
+
+            if (repository.findByEmail("jahnavi2108@gmail.com").isEmpty()) {
+
+                User user = new User();
+
+                user.setFullName("Jahnavi");
+                user.setEmail("jahnavi2108@gmail.com");
+                user.setPassword(encoder.encode("123456"));
+                user.setRole("USER");
+
+                repository.save(user);
+            }
+        };
+    }
+}
--- src/main/java/com/campusguard/backend/config/SecurityConfig.java
+++ src/main/java/com/campusguard/backend/config/SecurityConfig.java
@@ -12,13 +12,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                )
-                .httpBasic(Customizer.withDefaults());
-
+       http
+    .csrf(csrf -> csrf.disable())
+    .cors(cors -> {})
+    .authorizeHttpRequests(auth -> auth
+        .anyRequest().permitAll()
+    );
         return http.build();
     }
 }
