c1729bbb937e982aaa1607809555962552703563
SUKKABOTLA JAHNAVI <jahnavi2108@gmail.com>
2026-08-03 20:52:09 +0530

    Fix password encoder and add initializer logs

--- src/main/java/com/campusguard/backend/config/DataInitializer.java
+++ src/main/java/com/campusguard/backend/config/DataInitializer.java
@@ -14,16 +14,23 @@ public class DataInitializer {
     CommandLineRunner init(UserRepository repository, PasswordEncoder encoder) {
         return args -> {
 
+            System.out.println("=== DataInitializer Started ===");
+
             if (repository.findByEmail("jahnavi2108@gmail.com").isEmpty()) {
 
-                User user = new User();
+                System.out.println("Creating default user...");
 
+                User user = new User();
                 user.setFullName("Jahnavi");
                 user.setEmail("jahnavi2108@gmail.com");
                 user.setPassword(encoder.encode("123456"));
                 user.setRole("USER");
 
                 repository.save(user);
+
+                System.out.println("Default user created.");
+            } else {
+                System.out.println("User already exists.");
             }
         };
     }
--- src/main/java/com/campusguard/backend/config/SecurityConfig.java
+++ src/main/java/com/campusguard/backend/config/SecurityConfig.java
@@ -2,8 +2,9 @@ package com.campusguard.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,12 +13,18 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-       http
-    .csrf(csrf -> csrf.disable())
-    .cors(cors -> {})
-    .authorizeHttpRequests(auth -> auth
-        .anyRequest().permitAll()
-    );
+        http
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> {})
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
